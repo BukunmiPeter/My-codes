0c98f73ca5573c0edaa5d0f753ee1c32ccb391ff
BukunmiPeter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-07 11:35:38 +0100

    Add files via upload

---
+++ eventjs/events.js
@@ -0,0 +1,15 @@
+function ChangeDivone(){
+
+document.getElementById('Divone').style.backgroundColor="Pink";
+}
+
+
+function ChangeDivtwo(){
+  document.getElementById('Divtwo').style.backgroundColor="blue";
+  document.body.style.backgroundColor="grey";
+
+}
+
+function Changetext(){
+  document.getElementById('para').innerHTML="wow! all the text changed!!!!!!!";
+}
---
+++ stylingjswithcss.js
@@ -0,0 +1,7 @@
+function ChangeText(){
+
+document.getElementById("boringP").innerHTML= "This is the new, fun paragraph!!!";
+document.getElementById('boringP').style.backgroundColor="red";
+document.getElementById('boringP').style.border = "3px solid green";
+document.body.style.backgroundColor = "pink";
+}

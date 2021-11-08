dbce5d70db24521de392cbc8df1faec110a18149
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-08 21:55:05 +0100

    Add files via upload

---
+++ event2/event2.js
@@ -0,0 +1,10 @@
+function changebox1(){
+  document.getElementById('box1').style.backgroundColor="brown";
+  document.body.style.backgroundColor="gold";
+}
+
+
+function changebox2(){
+  document.getElementById('box2').style.backgroundColor="grey";
+  document.body.style.backgroundColor="green";
+}

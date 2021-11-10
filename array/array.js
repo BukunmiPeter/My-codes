2a7a33d67818421281b141356587590d1a3d844f
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-10 17:13:17 +0100

    Add files via upload

---
+++ array/array.js
@@ -0,0 +1,8 @@
+var myarra = [1,2,3,4,"hello",false, [5,6,7,8]]
+
+function show(){
+  var myarray= ["England", "France", "Belgium", "Spain",1,2,3,4,[5,6,7,"Germany"]]
+  var arrvalue= myarray.pop();
+
+  document.getElementById('showpara').innerHTML= arrvalue;
+}

10c82ffe7d68362f29c896f75691086b8d247945
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-11 12:41:26 +0100

    Add files via upload

---
+++ else if/elseif.js
@@ -0,0 +1,12 @@
+if (1==3) {
+  document.body.style.backgroundColor="Red"
+}
+else if (1==1) {
+  document.body.style.backgroundColor="brown"
+}
+else if (1==4) {
+  document.body.style.backgroundColor="green"
+}
+else {
+  document.body.style.backgroundColor="gold"
+}

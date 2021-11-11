d0180452973d6d1f4514535a5892cbf8acc8b347
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-11 11:26:39 +0100

    Add files via upload

---
+++ object/object.js
@@ -0,0 +1,10 @@
+
+var mycarobject = {color:"Red", maxspeed:"200 km/h", seats:2, wheels:4};
+var myobject= {name:"Peter", eyecolor:"white",skill:"developer"};
+var countrypopulation = {Engand:3000, france:2000, indonesia: 1500};
+myobject.enginesize="3L";
+function run(){
+  var value= myobject.enginesize;
+  document.getElementById('output').innerHTML=value;
+
+}

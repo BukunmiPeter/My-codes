f1bceb0cfb5441a8c2ab60d62c748d9b98d699a3
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-11 12:30:39 +0100

    Add files via upload

---
+++ ifelse/ifelse.js
@@ -0,0 +1,27 @@
+function ifElse() {
+  var correctpassword = "myPassword1234";
+  attemptedpassword ="myPassword134";
+  if (correctpassword==attemptedpassword){
+    alert("password correct-Access granted");
+  }else{alert("Wrong password - Access denied")}
+}
+
+
+function ifelse2(){
+  var personAge = 12;
+  if (personAge > 16){
+    alert("Your are allowed to drive.");
+  }else {alert("You are too young!!")
+
+  }
+}
+
+function ifelse3(){
+  var enoughpay = 50;
+  var paidprice = 49;
+  if(enoughpay<paidprice){
+    alert("Sufficient Pay! - Procced")
+  }else { alert("insufficient fund!")
+
+  }
+}

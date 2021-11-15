7547afc348bc908d2e9bfa09805fac9e446c0c7d
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-15 12:54:48 +0100

    Add files via upload

---
+++ for loop/forloop.js
@@ -0,0 +1,13 @@
+/*for (var i = 0; i < array.length; i++) {
+  array[i]
+}*/
+/*for (i=0; i<10; i++) {
+  alert("i is :" +i);
+}*/
+
+
+var arrfruit = ["Banana", "apple", "Orange", "Raspebery"];
+
+for(i=0; i<arrfruit.length; i++){
+  alert(arrfruit[i]);
+}

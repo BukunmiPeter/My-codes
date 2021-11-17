e4f9da9729042b6bb4ec50eff601170df0378d3e
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-17 11:52:46 +0100

    Add files via upload

---
+++ jquery1/jquery.js
@@ -0,0 +1,5 @@
+$("#backchange").on("click", function() {$("body").css("background-color", "green")})
+
+$("#bigfont").on("click", function()  {$(this).css("font-size", "20px")})
+
+$("#reverse").on("click", function() {$("p").css("direction","rtl")})

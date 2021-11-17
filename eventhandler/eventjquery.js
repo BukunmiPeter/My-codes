4c3ddc8f49af8ed4e1c70cb65861f2587925c0e8
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-17 12:53:49 +0100

    Add files via upload

---
+++ eventhandler/eventjquery.js
@@ -0,0 +1,9 @@
+$("#backchange").click(function(){$("body").css("background-color","red")})
+
+$("#bigfont").hover(function(){$(this).css("font-size", "20px")})
+
+$("#reverse").mouseleave(function() {$("p").css("direction","rtl")})
+
+$(".testkey").keydown(function(){$("p").html("You are typing in the input box")})
+
+$("#header").hover(function(){$("h1").css("text-align", "center")})

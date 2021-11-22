253ca498588a6af876743d51db6d9b4e9df155d6
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-22 14:39:46 +0100

    Add files via upload

---
+++ A few lists methods.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+List methods
+- append
+- remove
+-pop
+sort
+reverse
+
+List functons
+- len()
+-min()
+-max()
+"""
+
+
+# In[26]:
+
+
+my_list = [1,23,56,78,999,24,54,67,2233]
+my_listletters = ["r","t","a", "h", "g","z","l"]
+
+
+# In[9]:
+
+
+my_list.append(102)
+
+
+# In[28]:
+
+
+print(my_list)
+
+
+# In[19]:
+
+
+my_list.pop()
+
+
+# In[17]:
+
+
+my_list.append(23)
+
+
+# In[14]:
+
+
+my_list.reverse()
+
+
+# In[21]:
+
+
+my_listletters.sort()
+
+
+# In[22]:
+
+
+print(my_listletters)
+
+
+# In[27]:
+
+
+my_list.sort()
+
+
+# In[29]:
+
+
+len(my_list)
+
+
+# In[30]:
+
+
+min(my_list)
+
+
+# In[31]:
+
+
+max(my_list)
+
+
+# In[ ]:
+
+
+
+

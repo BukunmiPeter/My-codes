69d08aa92408737bffa16e0f4c6841c96541b671
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-22 13:14:33 +0100

    Add files via upload

---
+++ Dice rolling Simulator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import random as rn
+dice_one= rn.randint(1,6)
+dice_two = rn.randint(1,6)
+sum = dice_one + dice_two
+print("Dice values are {} and {}, and the total is {}".format(dice_one,dice_two,sum))
+
+
+# In[ ]:
+
+
+
+

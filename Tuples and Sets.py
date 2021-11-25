f26aff5b8880ecebf28c8be541e466f1498d7d99
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-11-25 13:15:11 +0100

    Add files via upload

---
+++ Tuples and Sets.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Tuples () -- This is a type of list that is immutable, i.e you cannot change any element in it
+Sets {} --- THis is a type of list that does not take replica.. or repetition of elements
+
+"""
+
+
+# In[1]:
+
+
+my_list= [23,45,67,88,9]
+
+
+# In[3]:
+
+
+my_list[2] = 100
+print(my_list)
+
+
+# In[6]:
+
+
+my_tuple = (1,2,3,4)
+
+
+# In[7]:
+
+
+my_tuple[2]
+
+
+# In[8]:
+
+
+my_tuple[2] = 34
+
+
+# In[15]:
+
+
+my_set= {23,55,66,77,88,6,55, "Gone", "Gone","gone"}
+
+
+# In[16]:
+
+
+print(my_set)
+
+
+# In[ ]:
+
+
+
+

c25220c0b7e3b43de8f9cdc63b247101b3c1f28e
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-12-02 09:22:40 +0100

    Add files via upload

---
+++ Recursive Functions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def recurs_func(number):
+    print("X is: {}". format(number))
+    number+=1
+    recurs_func(number)
+
+
+# In[2]:
+
+
+recurs_func(1)
+
+
+# In[ ]:
+
+
+
+

f390a846487eeece5c06545c21537028a8c103b7
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-12-06 14:47:02 +0100

    Add files via upload

---
+++ OOP creating a class.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+class My_class:
+        x=55
+        name = "Mr James Peter"
+        my_list_x = ["bread", "fruit", "Milk", "Junks"]
+        
+
+
+# In[3]:
+
+
+My_class.my_list_x
+
+
+# In[4]:
+
+
+My_class.x
+
+
+# In[ ]:
+
+
+
+

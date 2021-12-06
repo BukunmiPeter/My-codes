f49fdbc61de8b04ebb992a0f309379c689e409c5
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-12-06 13:56:44 +0100

    Add files via upload

---
+++ CustomModule.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def output_message():
+    print("Hey, this is working")
+
+
+# In[12]:
+
+
+def multiply():
+    user_input = int(input("Please provide a number. It will be multiplied by by 100"))
+    final_num = user_input*100
+    print("Your number is {} and answer is {}.".format(user_input,final_num))
+
+
+# In[13]:
+
+
+multiply()
+
+
+# In[ ]:
+
+
+
+

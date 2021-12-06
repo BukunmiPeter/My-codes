4d3ba45ea01b9a68771a6f66a4debeec39e1ec27
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-12-06 13:59:41 +0100

    Add files via upload

---
+++ import custommodule.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+output_message
+multiply
+
+"""
+
+
+# In[1]:
+
+
+import CustomModule
+
+
+# In[2]:
+
+
+CustomModule.output_message()
+
+
+# In[ ]:
+
+
+
+

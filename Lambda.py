a5ee6776fea80943272cc2581f3a24d623fb86e6
Adeyemo Oluwabukunmi Peter <93539093+BukunmiPeter@users.noreply.github.com>
2021-12-09 12:58:42 +0100

    Add files via upload

---
+++ Lambda.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+Functional Programming
+
+- Basic syntax
+- When is it useful, above using def
+
+"""
+
+
+# In[1]:
+
+
+lam_funct = lambda x: x+3
+
+
+# In[2]:
+
+
+lam_funct(10)
+
+
+# In[7]:
+
+
+my_lambda = lambda name : "Hello there, {}. How are you?".format(name)
+
+
+# In[8]:
+
+
+my_lambda("Peter")
+
+
+# In[ ]:
+
+
+
+
